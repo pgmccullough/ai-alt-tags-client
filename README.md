61972e71a0a98431437ecd30a70b9937c3753c15
Patrick Glendon McCullough <p@mccullo.ug>
2023-09-01 10:31:28 -0400

    update gitignore ffor sass

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Client for the AI Alt Tags API
 
 Stay tuned, it's Fresh.
+
+Lives at [ai-alt-tags.com](https://ai-alt-tags.com/)
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
         <title>AI Alt Tags | Effortless Accessibility</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
