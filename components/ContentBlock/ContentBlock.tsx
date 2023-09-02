bc32f410e76b836ee41cecddc4d6dd20a5d7d92f
Patrick Glendon McCullough <p@mccullo.ug>
2023-09-01 22:46:15 -0400

    API block init

---
+++ components/ContentBlock/ContentBlock.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "preact";
+
+export const ContentBlock = (props: JSX.HTMLAttributes<HTMLElement>) => {
+  return (
+    <section class="content-block">
+      <h2 class="content-block__h2">API Usage</h2>
+      
+    </section>
+  )
+}
